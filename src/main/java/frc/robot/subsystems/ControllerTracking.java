9e73e5d9542371dad7b0f6252163d3d99500b79a
Duel M <dueromoods@gmail.com>
2023-02-04 10:18:13 -0500

    Started naming process

--- src/main/java/frc/robot/subsystems/ControllerTracking.java
+++ src/main/java/frc/robot/subsystems/ControllerTracking.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -8,6 +9,7 @@ import frc.robot.RobotContainer.*;
 
 public class ControllerTracking {
     private static HIDType JoystickTypes[] = new HIDType[5];
+    private static String JoystickNames[] = new String[5];
 
     public static void updatePortNumbers() {
         GenericHID testHID;
@@ -15,6 +17,7 @@ public class ControllerTracking {
             testHID = new GenericHID(i);
             if (testHID.isConnected()) {
                 JoystickTypes[i] = testHID.getType();
+                JoystickNames[i] = DriverStation.getJoystickName(i);
             }
         }
         dynamicControllerXbox.object = new XboxController(indexOf(JoystickTypes, HIDType.kXInputGamepad));
