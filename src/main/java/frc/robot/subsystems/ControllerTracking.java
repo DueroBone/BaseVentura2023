247500ec193d687a4a17d53f5715ab51d0d701f0
Duel M <dueromoods@gmail.com>
2023-02-05 21:22:05 -0500

    I broke something

--- src/main/java/frc/robot/subsystems/ControllerTracking.java
+++ src/main/java/frc/robot/subsystems/ControllerTracking.java
@@ -5,13 +5,12 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.HIDType;
 import frc.robot.RobotContainer.*;
-//controller.getType()  Ps4 = kHIDGamepad  Xbox = kXInputGamepad  ATK3 = kHIDJoystick 
-class Xbox1 {
-  public static HIDType Type = HIDType.kXInputGamepad;
-  public static String Name = null;
-}
 
 public class ControllerTracking {
+  static HIDType isXbox = HIDType.kXInputGamepad;
+  static HIDType isPS4 = HIDType.kHIDGamepad;
+  static HIDType isJoystick = HIDType.kHIDJoystick;
+
   private static HIDType JoystickTypes[] = new HIDType[5];
   private static String JoystickNames[] = new String[5];
   public static void updatePortNumbers() {
@@ -25,40 +24,9 @@ public class ControllerTracking {
         JoystickNames[i] = DriverStation.getJoystickName(i);
       }
     }
-    dynamicControllerXbox1.object = new XboxController(portOf(JoystickTypes, Xbox1.Type, JoystickNames));
-    dynamicControllerPlaystation1.object= new XboxController(portOf(JoystickTypes, HIDType.kHIDGamepad, JoystickNames));
-    dynamicJoystick1.object = new Joystick(portOf(JoystickTypes, HIDType.kHIDJoystick, JoystickNames));
-  }
-  private static int portOf(HIDType[] HIDarray, HIDType type, String[] NAMEarray) {
-    int[] PossiblePorts = new int[HIDarray.length];
-    for (int i = 0; i < PossiblePorts.length; i++) {PossiblePorts[i] = 0;} //Fill possible with zeros
-
-    for (int i = 0; i < HIDarray.length; i++) {
-      if (HIDarray[i] == type) {
-        PossiblePorts[indexOfNumber(PossiblePorts, 0)] = i;
-      }
-    }
-
-    return 5;
-  }
-  private static int indexOfNumber(int[] IntArray, int number) {
-    for (int i = 0; i < IntArray.length; i++) {
-      if (IntArray[i] == number) {
-        return i;
-      }
-    }
-    return 5;
-  }
-  private static int[] removeEmptyFromArray(int[] InputArray) {
-    InputArray[1] = 0;
-    int[] output = new int[0];
-    int i2 = 0;
-    for (int i = 0; i < InputArray.length; i++) {
-      if (InputArray[i] != 0) {
-        output[i2++] = InputArray[i];
-      }
-    }
-    return output;
+    dynamicControllerXbox1.object = new XboxController(indexOfType(JoystickTypes, isXbox));
+    dynamicControllerPlaystation1.object= new XboxController(indexOfType(JoystickTypes, HIDType.kHIDGamepad));
+    dynamicJoystick1.object = new Joystick(indexOfType(JoystickTypes, HIDType.kHIDJoystick));
   }
   private static int indexOfType(HIDType[] HIDarray, HIDType type) {
     for (int i = 0; i < HIDarray.length; i++) {
