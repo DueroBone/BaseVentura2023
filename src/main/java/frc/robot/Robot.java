99f3bc1a233bfe851d252d026d3cbf8b7b5e973d
Duel M <dueromoods@gmail.com>
2023-02-11 11:00:07 -0500

    Quick Upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  public int CamWidth = 640;
+  public int CamHeight = 480;
 
   /*
    * This function is run when the robot is first started up and should be used for any
@@ -23,7 +25,7 @@ public class Robot extends TimedRobot {
             // autonomous chooser on the dashboard.
     if (!Robot.isSimulation()) {
       UsbCamera visionCamera = CameraServer.startAutomaticCapture();
-      visionCamera.setResolution(640, 480);
+      visionCamera.setResolution(CamWidth, CamHeight);
       visionCamera.setBrightness(15);
     }
   
--- src/main/java/frc/robot/subsystems/VisionPipeline.java
+++ src/main/java/frc/robot/subsystems/VisionPipeline.java
@@ -1,22 +1,23 @@
 package frc.robot.subsystems;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+/*import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.HashMap;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.HashMap;
-
-import org.opencv.core.*;
 import org.opencv.core.Core.*;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.*;
-import org.opencv.objdetect.*;
+import org.opencv.objdetect.*; */
 
 
 public class VisionPipeline {
+	public static final VisionPipeline m_visionPipeline = new VisionPipeline();
 
 	//Outputs
 	private Mat hslThresholdOutput = new Mat();
@@ -29,7 +30,7 @@ public class VisionPipeline {
 	}
 
 	//Primary Output Updater:
-	@Override public void process(Mat source0) {
+	public void process(Mat source0) {
 
 		//HSL Thresholding:
 		Mat hslThresholdInput = source0;
