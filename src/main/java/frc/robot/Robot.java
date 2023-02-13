397e1b717214a2dbd9311a5098d802cfe98a3618
Xander Bum <BlackHexFury896@protonmail.com>
2023-02-13 13:06:11 -0500

    Even more shenanigans

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.RobotContainer.dynamicControllerXbox1;
 import frc.robot.commands.RunInTeleop;
 
 public class Robot extends TimedRobot {
