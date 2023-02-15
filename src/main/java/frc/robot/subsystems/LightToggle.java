ac94a8cd78458d4f0688cd30498f03011426deea
Xander Bum <BlackHexFury896@protonmail.com>
2023-02-15 16:04:31 -0500

    Deez

---
+++ src/main/java/frc/robot/subsystems/LightToggle.java
@@ -0,0 +1,15 @@
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class LightToggle {
+
+    PWM pwm = new PWM(0);
+    // Set the PWM pulse width in milliseconds
+    pwm.setSpeed(1.5);
+
+    // Set the period of the PWM signal in milliseconds
+    pwm.setPeriodMs(10);
+
+    // Disable the PWM output
+    pwm.disable();
+}
