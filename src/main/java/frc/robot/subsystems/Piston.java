891cd98bd7c80e8421beff573b3173345c0a6340
DueroBone <dueromoods@gmail.com>
2023-02-15 16:09:30 -0500

    Fixed xander's errors

--- src/main/java/frc/robot/subsystems/Piston.java
+++ src/main/java/frc/robot/subsystems/Piston.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Piston {
